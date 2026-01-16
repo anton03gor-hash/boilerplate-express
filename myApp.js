41aa7d0436cadb51928a3449d970304509f88561
Anton Gorshkov <anton03gor@gmail.com>
2026-01-16 18:25:29 +0800

    serve json on specific route

--- myApp.js
+++ myApp.js
@@ -3,13 +3,22 @@ let app = express();
 // 1 
 // console.log("Hello World");
 
+// 4
+app.use("/public", express.static(__dirname + "/public"));
+
 // 2
-app.get("/", (req, res)=>{
-    // res.send("Hello Express");
-    // 3
-    absolutePath = __dirname + "/views/index.html";
-    res.sendFile(absolutePath);
-});
+// app.get("/", (req, res)=>{
+//     // res.send("Hello Express");
+//     // 3
+//     absolutePath = __dirname + "/views/index.html";
+//     res.sendFile(absolutePath);
+// });
+
+// 5
+app.get("/json", (req, res)=>{
+    res.json({"message": "Hello json"});
+})
+
 
 
 
